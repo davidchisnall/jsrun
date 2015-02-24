47c60fc1eb2be1015212d007a15dc055f60fcf9e
David Chisnall <theraven@theravensnest.org>
2015-02-24 15:30:09 +0000

    Delete some dead code left over from the duk_cmd example.

--- jsrun.c
+++ jsrun.c
@@ -29,98 +29,16 @@
  *  For maximum portability, compile with -DDUK_CMDLINE_BAREBONES
  */
 
-#if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || \
-    defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
-#ifndef DUK_CMDLINE_BAREBONES
-/* Force barebones mode on Windows. */
-#define DUK_CMDLINE_BAREBONES
-#endif
-#endif
-
-#ifdef DUK_CMDLINE_BAREBONES
-#define NO_READLINE
-#define NO_RLIMIT
-#define NO_SIGNAL
-#endif
-
-#define  GREET_CODE(variant)  \
-	"print(" \
-	"'((o) Duktape" variant "'" \
-	", " \
-	"Math.floor(Duktape.version / 10000) + '.' + " \
-	"Math.floor(Duktape.version / 100) % 100 + '.' + " \
-	"Duktape.version % 100" \
-	");"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef NO_SIGNAL
-#include <signal.h>
-#endif
-#ifndef NO_RLIMIT
-#include <sys/resource.h>
-#endif
-#ifndef NO_READLINE
 #include <readline/readline.h>
 #include <readline/history.h>
-#endif
 
 #include "jsrun.h"
 
-#define  MEM_LIMIT_NORMAL   (128*1024*1024)   /* 128 MB */
-#define  MEM_LIMIT_HIGH     (2047*1024*1024)  /* ~2 GB */
-#define  LINEBUF_SIZE       65536
-
-/* FIXME: additional modules should probably only be in some separate tool? */
-#if 0
-extern void duk_ncurses_register(duk_context *ctx);
-extern void duk_socket_register(duk_context *ctx);
-extern void duk_fileio_register(duk_context *ctx);
-#endif
-
 int interactive_mode = 0;
-
-#ifndef NO_RLIMIT
-static void set_resource_limits(rlim_t mem_limit_value) {
-	int rc;
-	struct rlimit lim;
-
-	rc = getrlimit(RLIMIT_AS, &lim);
-	if (rc != 0) {
-		fprintf(stderr, "Warning: cannot read RLIMIT_AS\n");
-		return;
-	}
-
-	if (lim.rlim_max < mem_limit_value) {
-		fprintf(stderr, "Warning: rlim_max < mem_limit_value (%d < %d)\n", (int) lim.rlim_max, (int) mem_limit_value);
-		return;
-	}
-
-	lim.rlim_cur = mem_limit_value;
-	lim.rlim_max = mem_limit_value;
-
-	rc = setrlimit(RLIMIT_AS, &lim);
-	if (rc != 0) {
-		fprintf(stderr, "Warning: setrlimit failed\n");
-		return;
-	}
-
-#if 0
-	fprintf(stderr, "Set RLIMIT_AS to %d\n", (int) mem_limit_value);
-#endif
-}
-#endif  /* NO_RLIMIT */
-
-#ifndef NO_SIGNAL
-static void my_sighandler(int x) {
-	fprintf(stderr, "Got signal %d\n", x);
-	
-}
-static void set_sigint_handler(void) {
-	(void) signal(SIGINT, my_sighandler);
-}
-#endif  /* NO_SIGNAL */
 
 static int get_stack_raw(duk_context *ctx) {
 	if (!duk_is_object(ctx, -1)) {
@@ -156,25 +74,6 @@ int wrapped_compile_execute(duk_context *ctx) {
 
 	comp_flags = 0;
 	duk_compile(ctx, comp_flags);
-
-#if 0
-	/* FIXME: something similar with public API */
-	if (interactive_mode) {
-		duk_hcompiledfunction *f = (duk_hcompiledfunction *) duk_get_hobject(ctx, -1);
-
-		if (f && DUK_HOBJECT_IS_COMPILEDFUNCTION((duk_hobject *) f)) {
-			fprintf(stdout, "[bytecode length %d opcodes, registers %d, constants %d, inner functions %d]\n",
-				(int) DUK_HCOMPILEDFUNCTION_GET_CODE_COUNT(f),
-				(int) f->nregs,
-				(int) DUK_HCOMPILEDFUNCTION_GET_CONSTS_COUNT(f),
-				(int) DUK_HCOMPILEDFUNCTION_GET_FUNCS_COUNT(f));
-			fflush(stdout);
-		} else {
-			fprintf(stdout, "[invalid compile result]\n");
-			fflush(stdout);
-		}
-	}
-#endif
 
 	duk_push_global_object(ctx);  /* 'this' binding */
 	duk_call_method(ctx, 0);
@@ -283,72 +182,6 @@ int handle_file(duk_context *ctx, const char *filename) {
 	return -1;
 }
 
-#ifdef NO_READLINE
-int handle_interactive(duk_context *ctx) {
-	const char *prompt = "duk> ";
-	char *buffer = NULL;
-	int retval = 0;
-	int rc;
-	int got_eof = 0;
-
-	duk_eval_string(ctx, GREET_CODE(" [no readline]"));
-	duk_pop(ctx);
-
-	buffer = (char *) malloc(LINEBUF_SIZE);
-	if (!buffer) {
-		fprintf(stderr, "failed to allocated a line buffer\n");
-		fflush(stderr);
-		retval = -1;
-		goto done;
-	}
-
-	while (!got_eof) {
-		size_t idx = 0;
-
-		fwrite(prompt, 1, strlen(prompt), stdout);
-		fflush(stdout);
-
-		for (;;) {
-			int c = fgetc(stdin);
-			if (c == EOF) {
-				got_eof = 1;
-				break;
-			} else if (c == '\n') {
-				break;
-			} else if (idx >= LINEBUF_SIZE) {
-				fprintf(stderr, "line too long\n");
-				fflush(stderr);
-				retval = -1;
-				goto done;
-			} else {
-				buffer[idx++] = (char) c;
-			}
-		}
-
-		duk_push_lstring(ctx, buffer, idx);
-		duk_push_string(ctx, "input");
-
-		interactive_mode = 1;  /* global */
-
-		rc = duk_safe_call(ctx, wrapped_compile_execute, 2 /*nargs*/, 1 /*nret*/);
-		if (rc != DUK_EXEC_SUCCESS) {
-			/* in interactive mode, write to stdout */
-			print_error(ctx, stdout);
-			retval = -1;  /* an error 'taints' the execution */
-		} else {
-			duk_pop(ctx);
-		}
-	}
-
- done:
-	if (buffer) {
-		free(buffer);
-		buffer = NULL;
-	}
-
-	return retval;
-}
-#else  /* NO_READLINE */
 int handle_interactive(duk_context *ctx) {
 	const char *prompt = "duk> ";
 	char *buffer = NULL;
@@ -408,7 +241,6 @@ int handle_interactive(duk_context *ctx) {
 
 	return retval;
 }
-#endif  /* NO_READLINE */
 
 
 int main(int argc, char *argv[]) {
@@ -419,16 +251,6 @@ int main(int argc, char *argv[]) {
 	int memlimit_high = 1;
 	int i;
 
-	/*
-	 *  Signal handling setup
-	 */
-
-#ifndef NO_SIGNAL
-	set_sigint_handler();
-
-	/* This is useful at the global level; libraries should avoid SIGPIPE though */
-	/*signal(SIGPIPE, SIG_IGN);*/
-#endif
 
 	/*
 	 *  Parse options
@@ -453,15 +275,7 @@ int main(int argc, char *argv[]) {
 		interactive = 1;
 	}
 
-	/*
-	 *  Memory limit
-	 */
 
-#ifndef NO_RLIMIT
-	set_resource_limits(memlimit_high ? MEM_LIMIT_HIGH : MEM_LIMIT_NORMAL);
-#else
-	(void) memlimit_high;  /* suppress warning */
-#endif
 
 	/*
 	 *  Create context and execute any argument file(s)
@@ -469,11 +283,6 @@ int main(int argc, char *argv[]) {
 
 	ctx = duk_create_heap_default();
 	init_default_objects(ctx);
-#if 0
-	duk_ncurses_register(ctx);
-	duk_socket_register(ctx);
-	duk_fileio_register(ctx);
-#endif
 
 	for (i = 1; i < argc; i++) {
 		char *arg = argv[i];
